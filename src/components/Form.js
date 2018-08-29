d7eb178721330cdad00a411261af06507126ba7a
naaltunian <naaltunian@icloud.com>
2018-08-29 17:39:32 -0400

    convert title, form, and weather components into stateless functions

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,30 @@
 import React from 'react';
 
-class Form extends React.Component {
-  render() {
-    return(
-      <div>
-        <form onSubmit={this.props.getWeather}>
-          <input type="text" name="city" placeholder="city"></input>
-          <input type="text" name="country" placeholder="country"></input>
-          <button>Get weather</button>
-        </form>
-      </div>
-    )
-  }
-}
+
+
+const Form = props => (
+  <form onSubmit={props.getWeather}>
+    <input type="text" name="city" placeholder="city"></input>
+    <input type="text" name="country" placeholder="country"></input>
+    <button>Get weather</button>
+  </form>
+);
 
 export default Form;
+
+
+// class Form extends React.Component {
+//   render() {
+//     return(
+//       <div>
+//         <form onSubmit={this.props.getWeather}>
+//           <input type="text" name="city" placeholder="city"></input>
+//           <input type="text" name="country" placeholder="country"></input>
+//           <button>Get weather</button>
+//         </form>
+//       </div>
+//     )
+//   }
+// }
+//
+// export default Form;
